3174f4802dfa3a50c1baee30e725cee5c55364cd
npolyak <nick.polyak@verizon.net>
2023-12-26 19:06:40 -0500

    some old project - made it compile and run, but did not fix it 100%.

---
+++ NP.MultiPlaformWindowDemo/App.axaml.cs
@@ -0,0 +1,27 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+namespace NP.MultiPlaformWindowDemo
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new StartupTestWindow();
+
+                desktop.MainWindow.Classes.Add("PlainCustomWindow");
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
