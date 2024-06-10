807d81d5aab934773029cbd2f2c27e6716f8cabd
npolyak <nick.polyak@verizon.net>
2024-06-09 23:27:46 -0400

    Grouping demo

---
+++ NP.DataGridGroupingDemo/NP.DataGridGroupingDemo/Views/MainView.axaml.cs
@@ -0,0 +1,39 @@
+using Avalonia.Collections;
+using Avalonia.Controls;
+using System.Text.RegularExpressions;
+
+namespace NP.DataGridGroupingDemo.Views;
+
+public partial class MainView : UserControl
+{
+    public People ThePeople { get; } = new People();
+
+    private bool _isGrouped = false;
+
+    private const string GroupingButtonName = "Group by Last Name";
+    private const string UnGroupingButtonName = "Ungroup";
+    public MainView()
+    {
+        InitializeComponent();
+
+        GroupByLastNameButton.Content = GroupingButtonName;
+        GroupByLastNameButton.Click += GroupByLastNameButton_Click;
+    }
+    private void GroupByLastNameButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        DataGridCollectionView collectionView = (DataGridCollectionView)TheDataGrid.ItemsSource;
+
+        if (_isGrouped)
+        {
+            collectionView.GroupDescriptions.Clear();
+            GroupByLastNameButton.Content = UnGroupingButtonName;
+        }
+        else
+        {
+            collectionView.GroupDescriptions.Add(new DataGridPathGroupDescription("LastName"));
+            GroupByLastNameButton.Content = GroupingButtonName;
+        }
+
+        _isGrouped = !_isGrouped;
+    }
+}
